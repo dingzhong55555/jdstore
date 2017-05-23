b2dadf74aeef36e0cd93c67ff7a629e6b049a89a
dingzhong <dingzhong55555@gmail.com>
2017-05-24 06:53:26 +0800

    add aasm_state to order

--- Gemfile
+++ Gemfile
@@ -45,6 +45,7 @@ gem 'simple_form'
 gem 'font-awesome-rails'
 gem 'carrierwave'
 gem 'mini_magick'
+gem 'aasm'
 
 group :development do
   # Access an IRB console on exception pages or by using <%= console %> anywhere in the code.
--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -2,7 +2,7 @@ class OrderMailer < ApplicationMailer
   def notify_order_placed(order)
     @order = order
     @user = order.user
-    @product_list = @order.product_lists
+    @product_lists = @order.product_lists
 
     mail(to: @user.email, subject: "[jDstore] 感谢您完成本次下单，一下是你的购物明细#{order.token}")
   end
