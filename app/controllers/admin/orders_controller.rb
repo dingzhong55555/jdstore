18bf8ba4af0cb5eacca9d1941411764ccf9d093d
dingzhong <dingzhong55555@gmail.com>
2017-05-24 07:40:28 +0800

    implement admin/orders show

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -7,4 +7,18 @@ class Admin::OrdersController < ApplicationController
   def index
     @order = Order.order("id DESC")
   end
+
+  def show
+    @order = Order.find(params[:id])
+    @product_lists = @order.product_lists
+  end
+
+  private
+
+  def admin_required
+    if !current_user.admin?
+      redirect_to "/", alert: "You are not admin"
+    end
+  end
+
 end
