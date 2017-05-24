d8421517c7150b46d18de8f96acda8e4514c63c9
dingzhong <dingzhong55555@gmail.com>
2017-05-25 07:56:33 +0800

    add ship/hipped/cancel/reture actiony to admin/order

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -5,12 +5,36 @@ class Admin::OrdersController < ApplicationController
   before_action :admin_required
 
   def index
-    @order = Order.order("id DESC")
+    @orders = Order.order("id DESC")
   end
 
   def show
     @order = Order.find(params[:id])
     @product_lists = @order.product_lists
+  end
+
+  def ship
+    @order = Order.find(params[:id])
+    @order.ship!
+    redirect_to :back
+  end
+
+  def shipped
+    @order = Order.find(params[:id])
+    @order.deliver!
+    redirect_to :back
+  end
+
+  def cancel
+    @order = Order.find(params[:id])
+    @order.cancel_order!
+    redirect_to :back
+  end
+
+  def reture
+    @order = Order.find(params[:id])
+    @order.reture_good!
+    redirect_to :back
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,14 @@ Rails.application.routes.draw do
   root 'products#index'
   namespace :admin do
     resources :products
-    resources :orders
+    resources :orders do
+      member do
+        post :cancel
+        post :ship
+        post :shipped
+        post :reture
+      end
+    end
   end
 
   devise_for :users
